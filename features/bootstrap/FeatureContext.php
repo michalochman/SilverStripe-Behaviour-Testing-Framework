5e32f1ecf7baf261a8de9ea2664746af674b9c34
Michal Ochman <ochman.d.michal@gmail.com>
2012-06-01 05:42:31 +0200

    adding PHPUnit requirements

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -7,6 +7,8 @@ use Behat\Gherkin\Node\PyStringNode,
 
 // Third party libraries
 require_once 'mink/autoload.php';
+require_once 'PHPUnit/Autoload.php';
+require_once 'PHPUnit/Framework/Assert/Functions.php';
 
 class FeatureContext extends BehatContext
 {
