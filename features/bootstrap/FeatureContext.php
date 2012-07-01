95d2ab515c7e012c7a7f64da3cacadd325eb40ea
Michał Ochman <michoch@gmail.com>
2012-07-01 19:52:41 +0200

    main FeatureContext cleanup

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,17 +1,5 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Context\Step,
-    Behat\Behat\Event\FeatureEvent,
-    Behat\Behat\Event\ScenarioEvent,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
-
-use Behat\SilverStripeExtension\Context\SilverStripeAwareContextInterface;
-
 use SilverStripe\Test\Behaviour\SilverStripeContext,
     SilverStripe\Test\Behaviour\BasicContext,
     SilverStripe\Test\Behaviour\LoginContext,
@@ -31,11 +19,8 @@ require_once 'PHPUnit/Framework/Assert/Functions.php';
  * Context automatically loaded by Behat.
  * Uses subcontexts to extend functionality.
  */
-class FeatureContext extends SilverStripeContext implements SilverStripeAwareContextInterface
+class FeatureContext extends SilverStripeContext
 {
-    protected $context;
-    protected $silverstripe;
-
     /**
      * Initializes context.
      * Every scenario gets it's own context object.
@@ -44,40 +29,11 @@ class FeatureContext extends SilverStripeContext implements SilverStripeAwareCon
      */
     public function __construct(array $parameters)
     {
-        // Initialize your context here
-        $this->context = $parameters;
-
         $this->useContext('BasicContext', new BasicContext($parameters));
         $this->useContext('LoginContext', new LoginContext($parameters));
         $this->useContext('CmsFormsContext', new CmsFormsContext($parameters));
         $this->useContext('CmsUiContext', new CmsUiContext($parameters));
-    }
 
-    public function setSilverstripe($silverstripe)
-    {
-        $this->silverstripe = $silverstripe;
-    }
-
-    /**
-     * @BeforeScenario
-     */
-    public function before(ScenarioEvent $event)
-    {
-        if (!isset($event->getContext()->silverstripe)) {
-            throw new LogicException('Context\'s $silverstripe has to be set when implementing SilverStripeAwareContextInterface.');
-        }
-
-        require_once($event->getContext()->silverstripe);
-        $dbname = SapphireTest::create_temp_db();
-        DB::set_alternative_database_name($dbname);
-    }
-
-    /**
-     * @AfterScenario
-     */
-    public function after(ScenarioEvent $event)
-    {
-        SapphireTest::kill_temp_db();
-        DB::set_alternative_database_name(null);
+        parent::__construct($parameters);
     }
 }
