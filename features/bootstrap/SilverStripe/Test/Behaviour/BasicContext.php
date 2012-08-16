99a683fc75923f53967e1990b5ca131aecb8017a
Michał Ochman <michoch@gmail.com>
2012-08-16 20:18:27 +0200

    BUG log complete ajax response on errors with stack trace

--- features/bootstrap/SilverStripe/Test/Behaviour/BasicContext.php
+++ features/bootstrap/SilverStripe/Test/Behaviour/BasicContext.php
@@ -64,7 +64,7 @@ window.onerror = function(msg) {
 if ('undefined' !== typeof window.jQuery) {
     window.jQuery('body').ajaxError(function(event, jqxhr, settings, exception) {
         if ('abort' === exception) return;
-        window.onerror(event.type + ': ' + settings.type + ' ' + settings.url + ' ' + exception);
+        window.onerror(event.type + ': ' + settings.type + ' ' + settings.url + ' ' + exception + ' ' + jqxhr.responseText);
     });
 }
 JS;
